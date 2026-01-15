58c7db9e8dfd14230e93bf35f6ee3f7d45b85dc1
harshkulyal9 <harshvardhankulyal0@gmail.com>
2026-01-15 19:14:18 +0530

    Add files via upload

---
+++ q1.py
@@ -0,0 +1,7 @@
+#Write a program to input two numbers and display their sum
+
+num_1=float(input("enter the first number:"))
+num_2=float(input("enter the second number"))
+
+sum=num_1+num_2
+print(f"the sum of two number is ={sum}")
---
+++ q2.py
@@ -0,0 +1,14 @@
+#Write a program to input two numbers and display their sum, difference, product, and quotient.
+
+num_1=float(input("enter the first number:"))
+num_2=float(input("enter the second number:"))
+
+sum=num_1+num_2
+difference=num_1-num_2
+product=num_1*num_2
+quotient=num_1/num_2
+
+print(f"sum of two number is :{sum} ")
+print(f"difference of two number is :{difference}")
+print(f"product of two number is:{product}")
+print(f"quotient of two number is:{quotient}")
