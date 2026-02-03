5405879aa855d9f3fb178454c4452a38e3c93b2d
harshkulyal9 <harshvardhankulyal0@gmail.com>
2026-02-03 20:47:50 +0530

    Add files via upload

---
+++ areatr.py.py
@@ -0,0 +1,11 @@
+a=float(input("enter the length of base:"))
+b=float(input("enter the length of height:"))
+
+area=0.5*a*b
+
+if area<0:
+    print("invalid")
+else:
+    print("the area of triangle is:",area)
+        
+        
---
+++ hypotenues.py.py
@@ -0,0 +1,6 @@
+p=int(input("enter the principle:"))
+r=float(input("enter the rate:"))
+t=float(input("enter the time:"))
+
+simple_interest=(p*r*t)/100
+print("the simple interest is:",simple_interest)
